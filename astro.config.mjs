34cdec90219e80faffae583f348ba1a11059a59b
Ishaq74 <ishaq.achour@gmail.com>
2025-08-10 17:31:05 +0200

    Suppression du fichier vercel.json inutile pour Astro sur Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,26 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import path from 'path';
+import tailwindcss from '@tailwindcss/vite';
+
+import react from '@astrojs/react';
+
+import icon from 'astro-icon';
+
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        '@components': path.resolve('./src/components'),
+        '@layouts': path.resolve('./src/layouts')
+      }
+    }
+  },
+
+  integrations: [react(), icon()],
+  adapter: vercel()
+});
